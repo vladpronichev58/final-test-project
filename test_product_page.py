5d7d3c0be49317c6d6323c5a98675d1502cd353b
Проничев Владислав Викторович <v.pronichev@r58ci.local>
2021-10-05 15:27:03 +0300

    add 3 tests see success messages

--- test_product_page.py
+++ test_product_page.py
@@ -14,16 +14,34 @@ import pytest
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 
-	'''					  
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+	'''	
+'''
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"])
+	
+def test_guest_can_add_product_to_basket(browser, link):
     product_page = ProductPage(browser, link)
     product_page.open()     
-    product_page.should_not_be_success_message()	
     product_page.should_be_add_cart_btn()
-    browser.implicitly_wait(10)
+    browser.implicitly_wait(5)
     product_page.solve_quiz_and_get_code()
     product_page.should_be_see_msg_add_basket()
     product_page.should_be_price_product_in_basket()
-    product_page.success_message_should_disappear()
-    time.sleep(30)	
+	'''
+@pytest.mark.parametrize('link', [pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/", marks=pytest.mark.xfail)])
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()     
+    product_page.should_be_add_cart_btn()
+    product_page.should_not_be_success_message()	
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"])
+def test_guest_cant_see_success_message(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()     
+    product_page.should_not_be_success_message()
+@pytest.mark.parametrize('link', [pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/", marks=pytest.mark.xfail)])
+def test_message_disappeared_after_adding_product_to_basket(browser, link):
+    product_page = ProductPage(browser, link)
+    product_page.open()     
+    product_page.should_be_add_cart_btn()
+    time.sleep(1)
+    product_page.success_message_should_disappear()	
